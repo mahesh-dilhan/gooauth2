eedafe77dc383b1a20b40ba804510a102828d9a2
mahesh <mail.mahesh.lk@gmail.com>
2022-01-24 18:38:22 +0800

    oauth2

--- client/main.go
+++ client/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/mahesh-dilhan/gooauth2/pkg/proto/credit"
+	"golang.org/x/oauth2"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/oauth"
+)
