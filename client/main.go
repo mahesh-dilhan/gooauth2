edc680c0d253b6a72a14d2de242510a2b1957f48
mahesh <mail.mahesh.lk@gmail.com>
2022-01-26 18:39:28 +0800

    oauth2

--- client/main.go
+++ client/main.go
@@ -15,4 +15,10 @@ import (
 func main() {
 	log.Println("Client running ...")
 
+	rpcCreds := oauth.NewOauthAccess(&oauth2.Token{AccessToken: "client-x-id"})
+	trnCreds, err := credentials.NewClientTLSFromFile("./client/cert/public.crt", "localhost")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 }
