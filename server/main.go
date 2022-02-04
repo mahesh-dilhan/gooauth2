281719ee23cf444de04e9c05aa0fa61262742cb4
mahesh <mail.mahesh.lk@gmail.com>
2022-02-04 08:28:29 +0800

    oauth2

--- server/main.go
+++ server/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+
+	"github.com/mahesh-dilhan/gooauth2/pkg/proto/credit"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
 type server struct {
 	credit.UnimplementedCreditServiceServer
 }
